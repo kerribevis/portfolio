3f4154035f0f0ab18f92bb9d176cf0509942f1ca
Kerri <89356691+kerribevis@users.noreply.github.com>
2021-10-18 11:49:36 -0400

    Add files via upload

---
+++ space/myscript.js
@@ -0,0 +1,81 @@
+function mySun() {
+  const x = document.getElementById("mySUN");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myMercury() {
+  const x = document.getElementById("myMERCURY");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myVenus() {
+  const x = document.getElementById("myVENUS");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myEarth() {
+  const x = document.getElementById("myEARTH");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myMars() {
+  var x = document.getElementById("myMARS");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myJupiter() {
+  const x = document.getElementById("myJUPITER");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function mySaturn() {
+  const x = document.getElementById("mySATURN");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function myUranus() {
+  const x = document.getElementById("myURANUS");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myNeptune() {
+  const x = document.getElementById("myNEPTUNE");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
