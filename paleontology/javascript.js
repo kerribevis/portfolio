1479593953470f9982b20943839a08c3bdbf7685
Kerri <89356691+kerribevis@users.noreply.github.com>
2021-10-27 11:40:23 -0400

    Add files via upload

---
+++ paleontology/javascript.js
@@ -0,0 +1,53 @@
+function functionOne() {
+  const x = document.getElementById("one");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function functionTwo() {
+  const x = document.getElementById("two");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function functionThree() {
+  const x = document.getElementById("three");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function functionFour() {
+  const x = document.getElementById("four");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function functionFive() {
+  const x = document.getElementById("five");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function functionSix() {
+  const x = document.getElementById("six");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
